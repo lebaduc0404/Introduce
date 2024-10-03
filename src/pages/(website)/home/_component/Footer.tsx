3e5c31281dd3b1769ade160c723352dc5993a378
duclbph33843 <duclbph33843@fpt.edu.vn>
2024-10-03 17:11:36 +0700

    Update ...6

--- src/pages/(website)/home/_component/Footer.tsx
+++ src/pages/(website)/home/_component/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <>
-      <section className="mt-[5em] mb-[1em]">
+      <section className="mt-[0.5em] mb-[0.5em]">
         <p className="text-[12px] my-[1em]">© Karyalokal Studio.</p>
       </section>
     </>

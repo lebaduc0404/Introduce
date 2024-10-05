83003ab796f5e060610c029f90efb01b017885a3
duclbph33843 <duclbph33843@fpt.edu.vn>
2024-10-05 14:46:16 +0700

    ...

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,9 @@
 import HomePage from "@/pages/(website)/home/page";
-import IntroducingPage from "@/pages/(website)/introducing/page";
-import IntroducingNewPage from "@/pages/(website)/introducingNew/page";
+// import IntroducingPage from "@/pages/(website)/introducing/page";
+// import IntroducingNewPage from "@/pages/(website)/introducingNew/page";
 import Detail from "@/pages/(website)/post/DetailPost";
 import ListPost from "@/pages/(website)/post/ListPost";
-import Terms from "@/pages/(website)/terms/page";
+// import Terms from "@/pages/(website)/terms/page";
 import { Route, Routes } from "react-router-dom";
 
 const Router = () => {
@@ -11,9 +11,9 @@ const Router = () => {
     <div>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/launch" element={<IntroducingPage />} />
+        {/* <Route path="/launch" element={<IntroducingPage />} />
         <Route path="/new" element={<IntroducingNewPage />} />
-        <Route path="/terms" element={<Terms />} />
+        <Route path="/terms" element={<Terms />} /> */}
         <Route path="/list/blog" element={<ListPost />} />
         <Route path="/detailblog/:id" element={<Detail />} />
       </Routes>
